91951fea84d9d9a24ab06a40a507bb9c10b8a68b
Jason <jason.daurus@gmail.com>
2016-04-12 22:49:45 +0800

    reducers.output format

--- app/reducers/output.js
+++ app/reducers/output.js
@@ -1,12 +1,16 @@
 import { format } from 'util'
 
 export default (state = '', action) => {
+
   switch (action.type) {
+
     case 'Output.append':
       if (action.method === 'clear') return ''
       return state + format(...action.args) + '\n'
+
     case 'Editor.update':
       return ''
+
     default:
       return state
   }
